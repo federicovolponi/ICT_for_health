606d9f8b358034bba47898ce50d98732f249e864
Federico <federicovolponi99@gmail.com>
2022-11-19 12:30:10 +0100

    update LAB03

--- LAB03/lab_ROC.py
+++ LAB03/lab_ROC.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.cluster as sk
+import scipy
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score,average_precision_score
 #plt.rcParams["font.family"] = "Times New Roman"
 
 # Function to evaluate sensitivity and specificity
@@ -14,17 +17,6 @@ def calcSensSpec(thresh, x0, x1):
     spec = n0/Nn
     return sens, spec
 
-# Function to evalute Area Under a Curve (AUC) using trapezoidal rule
-def calcAUC(n, sens):
-    h = 1/n
-    len_sens = len(sens)
-    x = np.linspace(0, len_sens, num=n, endpoint=False, dtype=int)
-    sum_buff = sens[x[0]]
-    for i in range(1, n-1):
-        sum_buff += 2*sens[x[i]]
-    sum_buff += sens[x[n - 1]]
-    AUC = h/2*sum_buff
-    return AUC
 # %%
 plt.close('all')
 xx = pd.read_csv("LAB03/covid_serological_results.csv")
@@ -51,8 +43,21 @@ x = Test2
 y = swab
 x0 = x[swab==0]
 x1= x[swab==1]
+# Find specificty and sensitivity for a given threshold and plot pdf's
+thresh = 5
 Np=np.sum(swab==1)
 Nn=np.sum(swab==0)
+n1=np.sum(x1>thresh)
+sens=n1/Np
+n0=np.sum(x0 < thresh)
+spec = n0/Nn
+x = [x0, x1]
+plt.figure()
+plt.hist(x, density=True)   #plot conditional pdf's
+plt.title("pdf for Test 1")
+#plt.show()
+
+# Find specificty and sensitivity for a threshold with the sorted values of Test 2
 thresh= np.sort(Test2)
 sens = np.zeros(len(thresh))    # sensitivity
 spec = np.zeros(len(thresh))    # specificity
@@ -66,27 +71,82 @@ plt.figure()
 plt.plot(thresh, spec, label="specifity - P(Tn|H)")
 plt.plot(thresh, sens, label="sensitivity - P(Tp|D)")
 plt.xlabel("threshold")
+plt.grid()
 plt.title("Test 2")
 plt.legend()
 #plt.show()
+
 # Plot ROC curve (Sensitivity vs Falsa alarm) for Test 2
 plt.figure()
 plt.plot(FA, sens)
 plt.xlabel("False alarm - P(Tp|H)")
 plt.ylabel("Sensitivity - P(Tn|H)")
-plt.title("ROC curve")
+plt.grid()
+plt.title("ROC curve - Test 2")
 #plt.show()
-# Calculate AUC
-AUC_test2 = calcAUC(n=100, sens=sens)
 
+# ROC curve using sklearn for test 2
+""" fpr,tpr,thresh = metrics.roc_curve(swab,Test2,pos_label=1)
+plt.figure
+plt.plot(fpr, tpr)
+plt.show() """
+
+# Calculate AUC for test 2
+AUC_test2 = np.abs(scipy.integrate.trapezoid(sens, FA))
+
+# Find the best threshold (False negative small, Sensitivity high)
+# P(D|Tp), P(D|Tn) versus threshold
+precision = sens/(sens + FA)    # P(D|Tp) precision
+P_D_Tn = spec/(spec + FA)
+plt.figure()
+plt.plot(thresh, precision, label= "Precision - P(D|Tp)")
+plt.plot(thresh,P_D_Tn, label="P(D|Tn)")
+plt.grid()
+plt.xlabel("Threshold")
+plt.title("P(D|Tp), P(D|Tn) versus threshold")
+plt.legend()
+plt.show()
+# P(H|Tp), P(H|Tn) versus threshold
+NPV = spec/(spec + 1 - sens)
+P_H_Tp = sens/(sens + 1 - sens)
+plt.figure()
+plt.plot(thresh, NPV, label= "NPV - P(H|Tn)")
+plt.plot(thresh,P_H_Tp, label="P(H|Tp)")
+plt.grid()
+plt.xlabel("Threshold")
+plt.title("P(H|Tp), P(H|Tn) versus threshold")
+plt.legend()
+plt.show()
+# P(D|Tp) versus P(H|Tn)
+plt.figure()
+plt.plot(precision, NPV)
+plt.grid()
+plt.xlabel("P(D|Tp)")
+plt.ylabel("P(H|Tn)")
+plt.title("P(D|Tp) versus P(H|Tn) - Test 2")
+plt.show()
 ######################### TEST 1 #####################################
 x = Test1
 y = swab
 x0 = x[swab==0]
 x1= x[swab==1]
+# Find specificty and sensitivity for a given threshold and plot pdf's
+thresh = 5
 Np=np.sum(swab==1)
 Nn=np.sum(swab==0)
+n1=np.sum(x1>thresh)
+sens=n1/Np
+n0=np.sum(x0 < thresh)
+spec = n0/Nn
+x = [x0, x1]
+plt.figure()
+plt.title("pdf for Test 1")
+plt.hist(x, density=True)   #plot conditional pdf's
+#plt.show()
+
+# Find specificty and sensitivity for a threshold with the sorted values of Test 1
 thresh= np.sort(Test1)
+thresh[0] = 0
 sens = np.zeros(len(thresh))    # sensitivity
 spec = np.zeros(len(thresh))    # specificity
 FA = np.zeros(len(thresh))  # False alarm
@@ -100,30 +160,44 @@ plt.plot(thresh, spec, label="specifity - P(Tn|H)")
 plt.plot(thresh, sens, label="sensitivity - P(Tp|D)")
 plt.xlabel("threshold")
 plt.title("Test 1")
+plt.grid()
 plt.legend()
 #plt.show()
-# Plot ROC curve (Sensitivity vs Falsa alarm) for Test 2
+# Plot ROC curve (Sensitivity vs Falsa alarm) for Test 1
 plt.figure()
 plt.plot(FA, sens)
 plt.xlabel("False alarm - P(Tp|H)")
 plt.ylabel("Sensitivity - P(Tn|H)")
-plt.title("ROC curve")
-#plt.show()
-AUC_test1 = calcAUC(n=100, sens=sens)
+plt.grid()
+plt.title("ROC curve - Test 1")
+plt.show()
 
-# Print AUC for test 1 and test 2
-print("AUC for test 1:", round(AUC_test1, 4))
-print("AUC for test 2:", round(AUC_test2,4))
-print("\n===================================================================\n")
-""" 
-n1=np.sum(x1>thresh)
-sens=n1/Np
-n0=np.sum(x0 < thresh)
-spec = n0/Nn
-x = [x0, x1]
-plt.hist(x, density=True)   #plot conditional pdf's
+# ROC curve using sklearn for test 1
+""" fpr,tpr,thresh = metrics.roc_curve(swab,Test2,pos_label=1)
+plt.figure
+plt.plot(fpr, tpr)
 plt.show()
  """
+# Calculate AUC for test 1
+AUC_test1 = np.abs(scipy.integrate.trapezoid(sens, FA))
+
+# Find the best threshold (False negative small, Sensitivity high)
+precision = sens/(sens + FA)    # P(D|Tp) precision
+P_D_Tn = spec/(spec + 1 - sens)
+plt.figure()
+plt.plot(thresh, precision, label= "Precision - P(D|Tp)")
+plt.plot(thresh,P_D_Tn, label="P(D|Tn)")
+plt.grid()
+plt.xlabel("Threshold")
+plt.legend()
+plt.show()
+
+
+# Print AUC for test 1 and test 2
+print("AUC for test 1:", round(AUC_test1, 3))
+print("AUC for test 2:", round(AUC_test2,3))
+print("\n===================================================================\n")
+
 data_descr = False
 # %% data analysis
 
