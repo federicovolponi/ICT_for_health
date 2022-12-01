e04e1196e4e8d5fd335ab6443d20a3a4bbdeddb1
Federico Volponi <federicovolponi99@gmail.com>
2022-12-01 11:21:32 +0100

    lab03: added title to charts

--- LAB03/lab_ROC.py
+++ LAB03/lab_ROC.py
@@ -19,7 +19,7 @@ def calcSensSpec(thresh, x0, x1):
 
 # %%
 plt.close('all')
-xx = pd.read_csv("covid_serological_results.csv")
+xx = pd.read_csv("ICT_for_health\LAB03\covid_serological_results.csv")
 # results from swab: 0= no illness, 1 = unclear, 2=illness
 swab = xx.COVID_swab_res.values
 swab[swab >= 1] = 1  # non reliable values are considered positive
@@ -54,7 +54,7 @@ spec = n0/Nn
 x = [x0, x1]
 plt.figure()
 plt.hist(x, density=True)   #plot conditional pdf's
-plt.title("pdf for Test 1")
+plt.title("pdf for Test 2")
 #plt.show()
 
 # Find specificty and sensitivity for a threshold with the sorted values of Test 2
@@ -113,7 +113,7 @@ plt.plot(thresh, P_D_Tp, label= "P(D|Tp)")
 plt.plot(thresh,P_D_Tn, label="P(D|Tn)")
 plt.grid()
 plt.xlabel("Threshold")
-plt.title("P(D|Tp), P(D|Tn) versus threshold")
+plt.title("P(D|Tp), P(D|Tn) versus threshold - Test 2")
 plt.legend()
 plt.show()
 # P(H|Tp), P(H|Tn) versus threshold
@@ -122,7 +122,7 @@ plt.plot(thresh, P_H_Tn, label= "P(H|Tn)")
 plt.plot(thresh,P_H_Tp, label="P(H|Tp)")
 plt.grid()
 plt.xlabel("Threshold")
-plt.title("P(H|Tp), P(H|Tn) versus threshold")
+plt.title("P(H|Tp), P(H|Tn) versus threshold - Test 2")
 plt.legend()
 plt.show()
 # P(D|Tp) versus P(H|Tn)
@@ -195,7 +195,35 @@ plt.show()
 AUC_test1 = np.abs(scipy.integrate.trapezoid(sens, FA))
 
 # Find the best threshold (False negative small, Sensitivity high)
-
+# P(D|Tp), P(D|Tn) versus threshold
+plt.figure()
+plt.plot(thresh, P_D_Tp, label= "P(D|Tp)")
+plt.plot(thresh,P_D_Tn, label="P(D|Tn)")
+plt.grid()
+plt.xlabel("Threshold")
+plt.title("P(D|Tp), P(D|Tn) versus threshold - Test 1")
+plt.legend()
+plt.show()
+# P(H|Tp), P(H|Tn) versus threshold
+plt.figure()
+plt.plot(thresh, P_H_Tn, label= "P(H|Tn)")
+plt.plot(thresh,P_H_Tp, label="P(H|Tp)")
+plt.grid()
+plt.xlabel("Threshold")
+plt.title("P(H|Tp), P(H|Tn) versus threshold - Test 1")
+plt.legend()
+plt.show()
+# P(D|Tp) versus P(H|Tn)
+plt.figure()
+plt.plot(P_D_Tp, P_H_Tn)
+plt.grid()
+plt.xlabel("P(D|Tp)")
+plt.ylabel("P(H|Tn)")
+plt.title("P(D|Tp) versus P(H|Tn) - Test 1")
+if prev == 0.01:
+    plt.axis([0, 1, 0.990, 1])
+plt.tight_layout()
+plt.show()
 
 # Print AUC for test 1 and test 2
 print("AUC for test 1:", round(AUC_test1, 3))
@@ -237,33 +265,3 @@ P_D_Tn = 1 - P_H_Tn
 P_D_Tp = (sens * prev)/(sens * prev + FA * healthy)
 # probabilty that a person with a positive test is healthy
 P_H_Tp = 1 - P_D_Tp
-
-# P(D|Tp), P(D|Tn) versus threshold
-plt.figure()
-plt.plot(thresh, P_D_Tp, label= "P(D|Tp)")
-plt.plot(thresh,P_D_Tn, label="P(D|Tn)")
-plt.grid()
-plt.xlabel("Threshold")
-plt.title("P(D|Tp), P(D|Tn) versus threshold")
-plt.legend()
-plt.show()
-# P(H|Tp), P(H|Tn) versus threshold
-plt.figure()
-plt.plot(thresh, P_H_Tn, label= "P(H|Tn)")
-plt.plot(thresh,P_H_Tp, label="P(H|Tp)")
-plt.grid()
-plt.xlabel("Threshold")
-plt.title("P(H|Tp), P(H|Tn) versus threshold")
-plt.legend()
-plt.show()
-# P(D|Tp) versus P(H|Tn)
-plt.figure()
-plt.plot(P_D_Tp, P_H_Tn)
-plt.grid()
-plt.xlabel("P(D|Tp)")
-plt.ylabel("P(H|Tn)")
-plt.title("P(D|Tp) versus P(H|Tn) - Test 1  ")
-if prev == 0.01:
-    plt.axis([0, 1, 0.990, 1])
-plt.tight_layout()
-plt.show()
